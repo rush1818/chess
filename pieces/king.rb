68abad86f217bf5f1ce9739ebbb581099c9c6884
Rushabh <rs@rushabhs.com>
2016-07-18 16:55:08 -0700

    Added the Knight and King classes

---
+++ pieces/king.rb
@@ -0,0 +1,15 @@
+require_relative 'stepable'
+
+class King < Piece
+
+  include Stepable
+
+  def symbol
+    '♚'.colorize(color)
+  end
+
+  def move_diffs
+    [[1,0], [0,1], [0,-1],[-1,0],[1,1],[1,-1], [-1,-1],[-1,1]]
+  end
+
+end
---
+++ pieces/pawn.rb
@@ -0,0 +1,15 @@
+require_relative 'stepable'
+
+class Pawn < Piece
+
+  include Stepable
+
+  def symbol
+    '♞'.colorize(color)
+  end
+
+  def move_diffs
+    [[1,2], [2,1], [-2,-1],[-2,1],[1,-2],[-1,-2],[-1,2],[2,-1]]
+  end
+
+end
